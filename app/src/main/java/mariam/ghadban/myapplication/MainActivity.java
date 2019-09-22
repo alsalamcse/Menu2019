d352bb91891ddf93dff47fa0eb74823a43afce9c
razi <razi@mograbi.net>
2019-09-22 14:54:53 +0300

    //

--- app/src/main/java/mariam/ghadban/myapplication/MainActivity.java
+++ app/src/main/java/mariam/ghadban/myapplication/MainActivity.java
@@ -3,13 +3,24 @@ package mariam.ghadban.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        MenuInflater inflater=getMenuInflater();
+        inflater.inflate(R.menu.my_menu);
+        return  true;
     }
 }
