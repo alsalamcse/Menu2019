07e9951dea692a939ccd00976ead63393be87d25
razi <razi@mograbi.net>
2019-09-29 08:54:50 +0300

    //

--- app/src/main/java/mariam/ghadban/myapplication/MainActivity.java
+++ app/src/main/java/mariam/ghadban/myapplication/MainActivity.java
@@ -2,9 +2,12 @@ package mariam.ghadban.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,7 +23,29 @@ public class MainActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu)
     {
         MenuInflater inflater=getMenuInflater();
-        inflater.inflate(R.menu.my_menu);
+        inflater.inflate(R.menu.my_menu,menu);
         return  true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        if (item.getItemId()==R.id.itmScreen1)
+        {
+            Intent intent=new Intent(this, ActivityScreen1.class);
+            startActivity(intent);
+        }
+
+        if (item.getItemId()==R.id.itmScreen2)
+        {
+            Intent intent=new Intent(this, ActivityScreen2.class);
+            startActivity(intent);
+        }
+
+       if (item.getItemId()==R.id.itmScreen1);
+        {
+           Toast.makeText(this, "ynet", Toast.LENGTH_SHORT).show();
+        }
+        return true;
     }
 }
